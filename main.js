733b381a962fb88011aed52bfe11e9ddecad5a4b
anaysaran-02152010 <102991400+anaysaran-02152010@users.noreply.github.com>
2022-07-02 22:32:03 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,249 @@
+
+            function mammaisthesecondbest()
+            {
+            
+                document.getElementById("msg1").innerHTML = "You are right!!!!.";
+            }   
+            // get reference to button
+            var btn1 = document.getElementById("button1");
+            // add event listener for the button, for action "click"
+            btn1.addEventListener("click", mammaisthesecondbest);
+
+
+
+            function daddaisthesecondbest()
+            {
+            
+                document.getElementById("msg2").innerHTML = "You are right!!!!.";
+            }   
+            // get reference to button
+            var btn7 = document.getElementById("button7");
+            // add event listener for the button, for action "click"
+            btn7.addEventListener("click", daddaisthesecondbest);
+
+
+
+            function annaisthesecondbest()
+            {
+            
+                document.getElementById("msg3").innerHTML = "You are right!!!!.";
+            }   
+            // get reference to button
+            var btn9 = document.getElementById("button9");
+            // add event listener for the button, for action "click"
+            btn9.addEventListener("click", annaisthesecondbest);
+
+
+
+            function ayanisthesecondbest()
+            {
+            
+                document.getElementById("msg4").innerHTML = "You are right!!!!.";
+            }   
+            // get reference to button
+            var btn16 = document.getElementById("button16");
+            // add event listener for the button, for action "click"
+            btn16.addEventListener("click", ayanisthesecondbest);
+
+
+
+            function avvaisthesecondbest()
+            {
+            
+                document.getElementById("msg5").innerHTML = "You are right!!!!.";
+            }   
+            // get reference to button
+            var btn18 = document.getElementById("button18");
+            // add event listener for the button, for action "click"
+            btn18.addEventListener("click", avvaisthesecondbest);
+
+
+
+            function ammamaisthesecondbest()
+            {
+            
+                document.getElementById("msg1").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn2 = document.getElementById("button2");
+            // add event listener for the button, for action "click"
+            btn2.addEventListener("click", ammamaisthesecondbest);
+
+
+
+            function dhruvaisthesecondbest()
+            {
+            
+                document.getElementById("msg1").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn3 = document.getElementById("button3");
+            // add event listener for the button, for action "click"
+            btn3.addEventListener("click", dhruvaisthesecondbest);
+
+
+
+            function vishvaisthesecondbest()
+            {
+            
+                document.getElementById("msg1").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn4 = document.getElementById("button4");
+            // add event listener for the button, for action "click"
+            btn4.addEventListener("click", vishvaisthesecondbest);
+
+
+
+            function swethaisthesecondbest()
+            {
+            
+                document.getElementById("msg2").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn5 = document.getElementById("button5");
+            // add event listener for the button, for action "click"
+            btn5.addEventListener("click", swethaisthesecondbest);
+
+
+
+            function adityaisthesecondbest()
+            {
+            
+                document.getElementById("msg2").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn6 = document.getElementById("button6");
+            // add event listener for the button, for action "click"
+            btn6.addEventListener("click", adityaisthesecondbest);
+
+
+
+            function siddharthisthesecondbest()
+            {
+            
+                document.getElementById("msg2").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn8 = document.getElementById("button8");
+            // add event listener for the button, for action "click"
+            btn8.addEventListener("click", siddharthisthesecondbest);
+
+
+
+            function gauthamisthesecondbest()
+            {
+            
+                document.getElementById("msg3").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn10 = document.getElementById("button10");
+            // add event listener for the button, for action "click"
+            btn10.addEventListener("click", gauthamisthesecondbest);
+
+
+
+            function sailyaisthesecondbest()
+            {
+            
+                document.getElementById("msg3").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn11 = document.getElementById("button11");
+            // add event listener for the button, for action "click"
+            btn11.addEventListener("click", sailyaisthesecondbest);
+
+
+
+            function vidyaisthesecondbest()
+            {
+            
+                document.getElementById("msg3").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn12 = document.getElementById("button12");
+            // add event listener for the button, for action "click"
+            btn12.addEventListener("click", vidyaisthesecondbest);
+
+
+
+            function madhavisthesecondbest()
+            {
+            
+                document.getElementById("msg4").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn13 = document.getElementById("button13");
+            // add event listener for the button, for action "click"
+            btn13.addEventListener("click", madhavisthesecondbest);
+
+
+
+            function hariisthesecondbest()
+            {
+            
+                document.getElementById("msg4").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn14 = document.getElementById("button14");
+            // add event listener for the button, for action "click"
+            btn14.addEventListener("click", hariisthesecondbest);
+
+
+
+            function krishnaisthesecondbest()
+            {
+            
+                document.getElementById("msg4").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn15 = document.getElementById("button15");
+            // add event listener for the button, for action "click"
+            btn15.addEventListener("click", krishnaisthesecondbest);
+
+
+
+            function aadiisthesecondbest()
+            {
+            
+                document.getElementById("msg5").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn17 = document.getElementById("button17");
+            // add event listener for the button, for action "click"
+            btn17.addEventListener("click", aadiisthesecondbest);
+
+
+
+            function ayushisthesecondbest()
+            {
+            
+                document.getElementById("msg5").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn19 = document.getElementById("button19");
+            // add event listener for the button, for action "click"
+            btn19.addEventListener("click", ayushisthesecondbest);
+
+
+
+            function rohanisthesecondbest()
+            {
+            
+                document.getElementById("msg5").innerHTML = "Sorry but you are incorrect :(";
+            }   
+            // get reference to button
+            var btn20 = document.getElementById("button20");
+            // add event listener for the button, for action "click"
+            btn20.addEventListener("click", rohanisthesecondbest);
+
+
+
+
+
+            
+
+             
+
+            
+            
